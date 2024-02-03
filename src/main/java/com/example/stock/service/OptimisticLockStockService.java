7ac88618432ccbd5bf7575527efdd5849268e282
hangyeol0531 <jhan0531@naver.com>
2024-02-04 00:12:47 +0900

    optimistic lock

--- src/main/java/com/example/stock/service/OptimisticLockStockService.java
+++ src/main/java/com/example/stock/service/OptimisticLockStockService.java
@@ -6,17 +6,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class PessimisticLockStockService {
+public class OptimisticLockStockService {
 
     private final StockRepository stockRepository;
 
-    public PessimisticLockStockService(StockRepository stockRepository) {
+    public OptimisticLockStockService(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
 
     @Transactional
-    public void decrease(Long id, Long quantity) {
-        Stock stock = stockRepository.findByIdWithOptimisticLock(id);
+    public void decrease(Long id, Long quantity){
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
         stock.decrease(quantity);
         stockRepository.save(stock);
     }
